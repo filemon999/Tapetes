dd4ac848ae81563670954aa0327ebbd0243c07e5
pato <gamm081007hgtrsga9@cbtis217.edu.mx>
2024-11-14 15:02:04 +0100

    Recien creado

---
+++ Tapetes/Tapetes/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2-PC15
+ * Date: 14/11/2024
+ * Time: 03:00 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Tapetes
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
